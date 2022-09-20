cd63cc4778c0f9f33a86e392ee888b96609db050
Andre Haupt <andrehaupt@me.com>
2022-09-20 16:14:54 +0200

    base setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
-import '../components/GlobalStyles'
+import GlobalStyle from '../components/GlobalStyles'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return
+  <>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
